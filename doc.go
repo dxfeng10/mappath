f63d0ebd8010687eaff8a3cc3698e82954aeb2d3
Ryan Faerman <ryan_faerman@ultimatesoftware.com>
2019-04-16 23:53:46 -0400

    add package doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package mappath exposes functionality to retreive values from generic
+// map[string]interface{} by path.
+//
+// The map can be arbitrarily complex and arbitrarily nested. This pairs nicely
+// with something like https://github.com/mitchellh/mapstructure
+package mappath
